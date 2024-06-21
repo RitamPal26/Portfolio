dfaee0baee239565da26dbb4f03251caaa0f0bbe
Ritam Pal <ritamjunior26@gmail.com>
2024-06-21 19:48:01 +0530

    com

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -37,7 +37,7 @@ const Home = () => {
           <img
             src={HeroImage}
             alt="my profile"
-            className="rounded-2xl mx-auto w-1/3 md:w-full"
+            className="rounded-2xl mx-auto w-3/4 md:w-half"
           />
         </div>
       </div>
